875947a7eb2cffb9b268b7614224dfad3354e90f
ShaneDsouza4 <shane.json96@gmail.com>
2024-07-07 01:38:01 -0400

    Cookies authentication, Logout feature

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const userRoute = require("./routes/user");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 const app = express();
 const PORT = 8000;
@@ -11,9 +13,13 @@ mongoose.connect("mongodb://localhost:27017/bloggApp").then(()=>console.log("Mon
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"))
 
 app.get("/", (req,res)=>{
-    res.render("home");
+    res.render("home",{
+        user: req.user
+    });
 })
 
 app.use("/user", userRoute)
