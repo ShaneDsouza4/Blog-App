b965e7e343d3a429eb5e9d63348b8831536f43da
ShaneDsouza4 <shane.json96@gmail.com>
2024-07-10 23:18:18 -0400

    Environment Variables Set

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const userRoute = require("./routes/user");
@@ -6,12 +8,15 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
+
 const Blog = require("./models/blog");
 
 const app = express();
-const PORT = 8000;
 
-mongoose.connect("mongodb://localhost:27017/bloggApp").then(()=>console.log("MongoDB Connected."));
+const PORT = process.env.PORT || 8000;
+
+
+mongoose.connect(process.env.MONGO_URL).then(()=>console.log("MongoDB Connected."));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
